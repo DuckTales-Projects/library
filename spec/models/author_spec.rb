3b0c58be4821d9b1b02e9c8bee5f7eb4c7d3daf6
James Toledo <jamesmacena14@gmail.com>
2022-02-13 15:18:45 -0300

    added new length validations

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -8,6 +8,21 @@ RSpec.describe Author, type: :model do
   end
 
   describe 'length validation' do
+    subject(:invalid_name) { Author.new(name: 'a' * 101) }
+    subject(:valid_name) { Author.new(name: 'a' * 100) }
+
     it { is_expected.to validate_length_of(:name).is_at_most(100) }
+
+    context 'when receive invalid names' do
+      it 'do not be valid' do
+        expect(invalid_name.valid?).to eq(false)
+      end
+    end
+
+    context 'when receive valid names' do
+      it 'be valid' do
+        expect(valid_name.valid?).to eq(true)
+      end
+    end
   end
 end
