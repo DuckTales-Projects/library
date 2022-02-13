1054cd65930e0e75e5870e70a06e34ae08343a73
James Toledo <jamesmacena14@gmail.com>
2022-02-13 13:05:13 -0300

    validation tests

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -3,4 +3,11 @@
 require 'rails_helper'
 
 RSpec.describe Author, type: :model do
+  describe 'presence validation' do
+    it { is_expected.to validate_presence_of(:name) }
+  end
+
+  describe 'length validation' do
+    it { is_expected.to validate_length_of(:name).is_at_most(100) }
+  end
 end
