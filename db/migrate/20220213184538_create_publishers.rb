6c5cf090f98b825f7f85162577e0d54ee74aacb8
James Toledo <jamesmacena14@gmail.com>
2022-02-13 15:54:11 -0300

    create table publisher

--- db/migrate/20220213184538_create_publishers.rb
+++ db/migrate/20220213184538_create_publishers.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class CreatePublishers < ActiveRecord::Migration[7.0]
   def change
     create_table :publishers do |t|
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps
     end
