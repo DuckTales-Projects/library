294fe17ea8a31fae653be3e6d5633fb29af5c52d
James Toledo <jamesmacena14@gmail.com>
2022-02-15 19:58:29 -0300

    create tests

--- spec/factories/publishers.rb
+++ spec/factories/publishers.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 FactoryBot.define do
   factory :publisher do
-    name { "MyString" }
+    name { Faker::Name.name }
   end
 end
--- spec/models/publisher_spec.rb
+++ spec/models/publisher_spec.rb
@@ -3,5 +3,29 @@
 require 'rails_helper'
 
 RSpec.describe Publisher, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'presence validation' do
+    it { is_expected.to validate_presence_of(:name) }
+  end
+
+  describe 'length validation' do
+    subject(:publisher) { build(:publisher, name:) }
+
+    let(:name) { 'a' * 100 }
+
+    it { is_expected.to validate_length_of(:name).is_at_most(100) }
+
+    context 'when receive invalid names' do
+      let(:name) { 'a' * 101 }
+
+      it 'do not be valid' do
+        expect(publisher.valid?).to eq false
+      end
+    end
+
+    context 'when receive valid names' do
+      it 'be valid' do
+        expect(publisher.valid?).to eq true
+      end
+    end
+  end
 end
