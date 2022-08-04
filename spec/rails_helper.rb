417865cfc7a3ef31ec6212837387eb55536587c7
James Toledo <jamesmacena14@gmail.com>
2022-08-04 15:58:19 -0300

    Requires support directory

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,8 +7,8 @@ require File.expand_path('../config/environment', __dir__)
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 
-require_relative 'support/factory_bot'
-require_relative 'support/shoulda_matchers'
+# requires support directory.
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
 
 begin
   ActiveRecord::Migration.maintain_test_schema!
