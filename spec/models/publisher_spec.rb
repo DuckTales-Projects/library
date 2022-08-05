7396644ac617bf7ef153302a944eef64d12fb401
James Toledo <jamesmacena14@gmail.com>
2022-08-05 20:10:03 -0300

    Remove redundant examples at publisher_spec

--- spec/models/publisher_spec.rb
+++ spec/models/publisher_spec.rb
@@ -3,29 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe Publisher, type: :model do
-  describe 'presence validation' do
+  describe '#valid' do
     it { is_expected.to validate_presence_of(:name) }
-  end
-
-  describe 'length validation' do
-    subject(:publisher) { build(:publisher, name:) }
-
-    let(:name) { 'a' * 100 }
-
     it { is_expected.to validate_length_of(:name).is_at_most(100) }
-
-    context 'when receive invalid names' do
-      let(:name) { 'a' * 101 }
-
-      it 'do not be valid' do
-        expect(publisher.valid?).to eq false
-      end
-    end
-
-    context 'when receive valid names' do
-      it 'be valid' do
-        expect(publisher.valid?).to eq true
-      end
-    end
   end
 end
