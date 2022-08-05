045798177ffee67e9207c60c6df107e687038357
James Toledo <jamesmacena14@gmail.com>
2022-08-05 20:12:41 -0300

    clean comments at requests/book_spec

--- spec/requests/book_spec.rb
+++ spec/requests/book_spec.rb
@@ -2,11 +2,5 @@
 
 require 'rails_helper'
 
-# RSpec.describe 'Books', type: :request do
-#   describe 'GET /create' do
-#     it 'returns http success' do
-#       get '/book/create'
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
-# end
+RSpec.describe 'Books', type: :request do
+end
